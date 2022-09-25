270d3b4457c1951d20bba06ae28f3bd904f2d01b
Michael Gillett <michaelgillett19@gmail.com>
2022-09-24 23:42:18 -0700

    stink

--- bot.py
+++ bot.py
@@ -157,9 +157,10 @@ async def self(interaction: discord.Interaction, query: str):
     #check if conor or justin
     if (interaction.author.id in [105884992055349248, 415407957371781123]):
 	await interaction.response.send_message("Nope.")
+	return
 
     #check if banned words
-    if ((query.find("blackface")) != -1 or (query.find("black face") != -1)):
+    if ((query.find("blackface") != -1) or (query.find("black face") != -1)):
 	await interaction.response.send_message("Nope.", ephemeral=True)
 	return
 
